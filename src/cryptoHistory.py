e1130d986be8a7bed470500cb17a498e9ec792d0
Bhanu Suraj Malla <bsmalla@asu.edu>
2024-09-29 18:31:05 -0700

    refactor cryptoHistory to use pycoingecko instead of Historic-Crypto library.

--- src/cryptoHistory.py
+++ src/cryptoHistory.py
@@ -1,32 +1,36 @@
-from Historic_Crypto import HistoricalData
-from datetime import datetime, timedelta
+from pycoingecko import CoinGeckoAPI
 import pandas as pd
 import os
-pd.DataFrame.append = pd.DataFrame._append
+from datetime import datetime, timedelta
 
-REFRESH_ALL_DATA = False
+cg = CoinGeckoAPI()
+
 CRYPTO_TICKER = "ETH-USD"
-
+PYCOINGECKO_TICKER = "ethereum"
+REFRESH_ALL_DATA = False
 
 class CryptoHistory:
 
     def __init__(self):
         self.refresh_all_data = REFRESH_ALL_DATA
         self.crypto_ticker = CRYPTO_TICKER
+        self.pycoingecko_ticker = PYCOINGECKO_TICKER
 
     def _load_data(self, start_date=None):
         if self.refresh_all_data:
-            new = HistoricalData(self.crypto_ticker, 86400, start_date="2010-01-01",
-                                 end_date=datetime.now().strftime("%Y-%m-%d-%H-%M")).retrieve_data()
+            new = cg.get_coin_market_chart_range_by_id(self.pycoingecko_ticker, vs_currency='usd',
+                                                      from_timestamp=1262304000,  # Start date: 2010-01-01
+                                                      to_timestamp=int(datetime.now().timestamp()))
         else:
-            new = HistoricalData('ETH-USD',86400, start_date=start_date,
-                                  end_date=datetime.now().strftime("%Y-%m-%d-%H-%M")).retrieve_data()
-        new = new.reset_index()
-        new = new[["time", "close"]]
-        new["time"] = new["time"].dt.date
-        new.columns = ["Date", "Price"]
+            start_timestamp = datetime.strptime(start_date, "%Y-%m-%d-%H-%M").timestamp()
+            new = cg.get_coin_market_chart_range_by_id("ethereum", vs_currency='usd',
+                                                      from_timestamp=start_timestamp,
+                                                      to_timestamp=int(datetime.now().timestamp()))
 
-        return new
+        prices = pd.DataFrame(new['prices'], columns=["time", "Price"])
+        prices["time"] = pd.to_datetime(prices["time"], unit='ms').dt.date
+        prices.columns = ["Date", "Price"]
+        return prices
 
     def update_data(self):
         if self.refresh_all_data:
@@ -43,9 +47,9 @@ class CryptoHistory:
         new.to_csv(f"{os.getcwd()}/data/{self.crypto_ticker}.csv", index=False)
         return new
 
-def main():
-    ch = CryptoHistory()
-    ch.update_data()
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     ch = CryptoHistory()
+#     ch.update_data()
+#
+# if __name__ == "__main__":
+#     main()
