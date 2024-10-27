1ec0471acc1d0eddba600ec7cdbc03d6d354390d
Bhanu Suraj Malla <bsmalla@asu.edu>
2024-10-26 20:11:55 -0700

    srp bill load

--- src/load_bill.py
+++ src/load_bill.py
@@ -1,5 +1,6 @@
 from expenseLoader import ExpenseLoader
 from gmailScraper import GmailScraper
+from srpScraper import SrpScraper
 import os
 
 
@@ -10,6 +11,9 @@ if os.environ.get("JOB") == "Rent":
     amount = 2750
 elif os.environ.get("JOB") == "Internet Bill":
     amount = 51.22
+elif os.environ.get("JOB") == "Electricity Bill":
+    ss = SrpScraper("https://www.srpnet.com/")
+    amount = ss.get_bill()
 else:
     amount = gs.scrape_bill()
 el.add_expense(amount, os.environ.get("JOB"))
